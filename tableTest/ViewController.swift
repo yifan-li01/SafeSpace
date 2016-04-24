38448f3ec9622ee61f5765a1487ddaca6371cbf1
Alan Li <alan.li.995@gmail.com>
2016-04-23 18:46:03 -0700

    Initial Commit

---
+++ tableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableTest
+//
+//  Created by Alan Li on 4/23/16.
+//  Copyright © 2016 Alan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
